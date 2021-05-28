b97babd424e902a5d0c837baf77b3b84edcb26b0
Wojciech Cichocki <cichockiwojciech97@gmail.com>
2021-05-28 22:01:36 +0200

    Remove debt overflow handler

--- programs/exchange/src/lib.rs
+++ programs/exchange/src/lib.rs
@@ -1252,8 +1252,6 @@ pub enum ErrorCode {
     NoRewards,
     #[msg("Invalid fund_account")]
     FundAccountError,
-    #[msg("Debt of assets is overflow")]
-    DebtOverflow,
 }
 
 // Access control modifiers.
