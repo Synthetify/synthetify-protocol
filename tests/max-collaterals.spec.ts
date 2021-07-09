dc7ac1cbc8f4b91e2fa0fa1886a448a91e402779
Mateusz Zając <matzayonc@gmail.com>
2021-07-09 12:34:18 +0200

    changed to synchronous to ensure same order

--- tests/max-collaterals.spec.ts
+++ tests/max-collaterals.spec.ts
@@ -54,6 +54,8 @@ describe('max collaterals', () => {
   let stakingFundAccount: PublicKey
   let CollateralTokenMinter: Account = wallet
   let nonce: number
+  let tokens: Token[] = []
+
   before(async () => {
     const [_mintAuthority, _nonce] = await anchor.web3.PublicKey.findProgramAddress(
       [SYNTHETIFY_ECHANGE_SEED],
@@ -72,6 +74,7 @@ describe('max collaterals', () => {
       payer: wallet,
       mintAuthority: CollateralTokenMinter.publicKey
     })
+    tokens.push(collateralToken)
     snyReserve = await collateralToken.createAccount(exchangeAuthority)
     snyLiquidationFund = await collateralToken.createAccount(exchangeAuthority)
     stakingFundAccount = await collateralToken.createAccount(exchangeAuthority)
@@ -97,6 +100,7 @@ describe('max collaterals', () => {
     })
     assetsList = data.assetsList
     usdToken = data.usdToken
+    tokens.push(usdToken)
 
     await exchange.init({
       admin: EXCHANGE_ADMIN.publicKey,
@@ -130,33 +134,36 @@ describe('max collaterals', () => {
   })
   it('fill assests', async () => {
     const tokenSpecificData = [
-      { decimals: 8, price: 50000, limit: new BN(1e12) },
-      { decimals: 4, price: 20, limit: new BN(1e8) },
-      { decimals: 6, price: 2, limit: new BN(1e14) },
-      { decimals: 4, price: 20, limit: new BN(1e8) },
-      { decimals: 6, price: 8, limit: new BN(1e10) }
+      { decimals: 8, price: 50000, limit: new BN(1e12) } // BTC
     ]
     for (let i = tokenSpecificData.length; i < ASSET_LIMIT - 2; i++) {
       tokenSpecificData.push({ decimals: 6, price: 2, limit: new BN(1e12) })
     }
 
-    const assetsListBefore = exchange.getAssetsList(assetsList)
+    const assetsListBefore = await exchange.getAssetsList(assetsList)
     assert.ok(tokenSpecificData.length == ASSET_LIMIT - 2)
     assert.ok((await assetsListBefore).assets.length)
 
-    const tokens = await Promise.all(
-      tokenSpecificData.map(async (specificData) => {
-        createCollateralToken({
+    let createdTokens = []
+
+    // must be synchronous to ensure same order
+    for (let tokenData of tokenSpecificData)
+      createdTokens.push(
+        await createCollateralToken({
           exchange,
           exchangeAuthority,
           oracleProgram,
           connection,
           wallet,
-          ...specificData
+          ...tokenData
         })
-      })
-    )
+      )
+    tokens = tokens.concat(createdTokens)
 
-    assert.ok(tokens.length == tokenSpecificData.length)
+    const assetsListAfter = await exchange.getAssetsList(assetsList)
+
+    assert.ok(createdTokens.length == tokenSpecificData.length)
+    assert.ok(assetsListAfter.head == ASSET_LIMIT)
+    console.log(assetsListAfter)
   })
 })
