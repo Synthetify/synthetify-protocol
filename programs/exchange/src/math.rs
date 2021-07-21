d52dc3a25624a36b57ca31835d958a7d2e0f8b08
Mateusz Zając <matzayonc@gmail.com>
2021-07-21 11:52:39 +0200

    calculate_debt error test fix

--- programs/exchange/src/math.rs
+++ programs/exchange/src/math.rs
@@ -658,42 +658,45 @@ mod tests {
             }
         }
     }
-    //     #[test]
-    //     fn test_calculate_debt_error() {
-    //         let slot = 100;
-    //         let mut assets_list = AssetsList {
-    //             ..Default::default()
-    //         };
-    //         let asset_1 = Asset {
-    //             price: 10 * 10u64.pow(PRICE_OFFSET.into()),
-    //             last_update: slot - 10,
-    //             synthetic: Synthetic {
-    //                 decimals: 8,
-    //                 supply: 100 * 10u64.pow(8),
-    //                 ..Default::default()
-    //             },
-    //             feed_address: Pubkey::new_unique(),
-    //             ..Default::default()
-    //         };
-    //         // debt 1000
-    //         let asset_2 = Asset {
-    //             price: 12 * 10u64.pow(PRICE_OFFSET.into()),
-    //             last_update: 100,
-    //             synthetic: Synthetic {
-    //                 decimals: 8,
-    //                 supply: 200 * 10u64.pow(8),
-    //                 ..Default::default()
-    //             },
-    //             ..Default::default()
-    //         };
-    //         assets_list.append(asset_1);
-    //         assets_list.append(asset_2);
-    //         let assets_ref = RefCell::new(assets_list);
+    #[test]
+    fn test_calculate_debt_error() {
+        let slot = 100;
+        let mut assets_list = AssetsList {
+            ..Default::default()
+        };
 
-    //         // debt 2400
-    //         let result = calculate_debt(&assets_ref.borrow_mut(), slot, 0);
-    //         assert!(result.is_err());
-    //     }
+        // debt 1400
+        assets_list.append_asset(Asset {
+            price: 10 * 10u64.pow(PRICE_OFFSET.into()),
+            last_update: slot - 10,
+            ..Default::default()
+        });
+        assets_list.append_synthetic(Synthetic {
+            supply: 100 * 10u64.pow(8),
+            decimals: 8,
+            asset_index: assets_list.head_assets as u8 - 1,
+            ..Default::default()
+        });
+
+        // debt 1000
+        assets_list.append_asset(Asset {
+            price: 12 * 10u64.pow(PRICE_OFFSET.into()),
+            last_update: 100,
+            ..Default::default()
+        });
+        assets_list.append_synthetic(Synthetic {
+            supply: 200 * 10u64.pow(8),
+            decimals: 8,
+            asset_index: assets_list.head_assets as u8 - 1,
+            ..Default::default()
+        });
+
+        let assets_ref = RefCell::new(assets_list);
+
+        // debt 2400
+        let result = calculate_debt(&assets_ref.borrow_mut(), slot, 0);
+        assert!(result.is_err());
+    }
     //     #[test]
     //     fn test_calculate_user_debt() {
     //         {
