d911dc391dbc2330d4056aef8a43133f03c0f6cc
Mateusz Zając <matzayonc@gmail.com>
2021-07-22 09:58:00 +0200

    removed check_liquidation

--- programs/exchange/src/utils.rs
+++ programs/exchange/src/utils.rs
@@ -33,24 +33,6 @@ pub fn div_up(a: u128, b: u128) -> u128 {
         .unwrap();
 }
 
-pub fn check_liquidation(
-    user_collateral: u64,
-    user_debt: u64,
-    liquidation_threshold: u8,
-) -> Result<()> {
-    let is_safe = (user_debt as u128)
-        .checked_mul(liquidation_threshold as u128)
-        .unwrap()
-        .checked_div(100)
-        .unwrap()
-        >= user_collateral as u128;
-    if is_safe {
-        return Ok(());
-    } else {
-        return Err(ErrorCode::InvalidLiquidation.into());
-    }
-}
-
 pub fn adjust_staking_rounds(state: &mut RefMut<State>, slot: u64) {
     if slot <= state.staking.next_round.start {
         return;
@@ -187,26 +169,9 @@ pub fn get_user_sny_collateral_balance(
 #[cfg(test)]
 mod tests {
 
+    use super::*;
     use std::{cell::RefCell, u64};
 
-    use super::*;
-    #[test]
-    fn test_check_liquidation() {
-        {
-            let result = check_liquidation(1000, 499, 200);
-            match result {
-                Ok(_) => assert!(false, "Shouldn't check"),
-                Err(_) => assert!(true),
-            }
-        }
-        {
-            let result = check_liquidation(1000, 500, 200);
-            match result {
-                Ok(_) => assert!(true),
-                Err(_) => assert!(false, "Shouldn't check"),
-            }
-        }
-    }
     #[test]
     fn adjust_staking_account_test() {
         let staking_round_length = 100;
