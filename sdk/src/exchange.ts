2a7dbe56172fcda1ad5ce5beeb237f827a4e36f6
Wojciech Cichocki <cichockiwojciech97@gmail.com>
2021-08-02 10:46:51 +0200

    Add withdrawAccumulatedDebtInterestInstruction to SDK

--- sdk/src/exchange.ts
+++ sdk/src/exchange.ts
@@ -763,8 +763,20 @@ export class Exchange {
       }
     })) as TransactionInstruction
   }
-  public async withdrawSwapTaxInstruction({ amount, to }: WithdrawSwapTaxInstruction) {
+  public async withdrawSwapTaxInstruction({ amount, to }: AdminWithdraw) {
     return (await this.program.instruction.withdrawSwapTax(amount, {
+      accounts: {
+        state: this.stateAddress,
+        admin: this.state.admin,
+        exchangeAuthority: this.exchangeAuthority,
+        usdToken: this.assetsList.synthetics[0].assetAddress,
+        to: to,
+        tokenProgram: TOKEN_PROGRAM_ID
+      }
+    })) as TransactionInstruction
+  }
+  public async withdrawAccumulatedDebtInterestInstruction({ amount, to }: AdminWithdraw) {
+    return (await this.program.instruction.withdrawAccumulatedDebtInterestInstruction(amount, {
       accounts: {
         state: this.stateAddress,
         admin: this.state.admin,
@@ -888,7 +900,7 @@ export interface AddNewAssetInstruction {
   assetsList: PublicKey
   assetFeedAddress: PublicKey
 }
-export interface WithdrawSwapTaxInstruction {
+export interface AdminWithdraw {
   amount: BN
   to: PublicKey
 }
