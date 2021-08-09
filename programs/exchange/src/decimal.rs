0dd444c10dc0ae437c1bd9e46aecabb4a5e4657e
Mateusz Zając <matzayonc@gmail.com>
2021-08-09 13:42:31 +0200

    sub and div for decimal

--- programs/exchange/src/decimal.rs
+++ programs/exchange/src/decimal.rs
@@ -55,6 +55,14 @@ impl Mul<Decimal> for Decimal {
         };
     }
 }
+impl Mul<u128> for Decimal {
+    fn mul(self, value: u128) -> Self {
+        return Self {
+            val: self.val.checked_mul(value).unwrap(),
+            scale: self.scale,
+        };
+    }
+}
 impl MulInverse<Decimal> for Decimal {
     fn mul_inverse(self, value: Decimal) -> Self {
         return Self {
@@ -86,19 +94,57 @@ impl Add<u64> for Decimal {
         })
     }
 }
+impl Sub<Decimal> for Decimal {
+    fn sub(self, value: Decimal) -> Result<Self> {
+        require!(self.scale == value.scale, DifferentScale);
+        Ok(Self {
+            val: self.val.checked_sub(value.val).unwrap(),
+            scale: self.scale,
+        })
+    }
+}
+impl Sub<u64> for Decimal {
+    fn sub(self, value: u64) -> Result<Self> {
+        Ok(Self {
+            val: self.val.checked_sub(value.into()).unwrap(),
+            scale: self.scale,
+        })
+    }
+}
+impl Div<Decimal> for Decimal {
+    fn div(self, other: Decimal) -> Self {
+        Self {
+            val: self
+                .val
+                .checked_mul(10u128.pow(self.scale.into()))
+                .unwrap()
+                .checked_div(other.val)
+                .unwrap(),
+            scale: self.scale,
+        }
+    }
+}
+impl DivUp<Decimal> for Decimal {
+    fn div_up(self, other: Decimal) -> Self {
+        self.add(other.sub(1).unwrap()).unwrap().div(other)
+    }
+}
 impl Into<u64> for Decimal {
     fn into(self) -> u64 {
         self.val.try_into().unwrap()
     }
 }
-pub trait Sub: Sized {
-    fn sub(self, rhs: Self) -> Self;
+pub trait Sub<T>: Sized {
+    fn sub(self, rhs: T) -> Result<Self>;
 }
 pub trait Add<T>: Sized {
     fn add(self, rhs: T) -> Result<Self>;
 }
 pub trait Div<T>: Sized {
     fn div(self, rhs: T) -> Self;
+}
+pub trait DivUp<T>: Sized {
+    fn div_up(self, rhs: T) -> Self;
 }
 pub trait Mul<T>: Sized {
     fn mul(self, rhs: T) -> Self;
