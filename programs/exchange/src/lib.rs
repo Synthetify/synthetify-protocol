180e3e26f8d5c5c150b1fe4ac57c6e6699addfb9
Wojciech Cichocki <cichockiwojciech97@gmail.com>
2021-09-30 14:53:06 +0200

    Change confidence threshold

--- programs/exchange/src/lib.rs
+++ programs/exchange/src/lib.rs
@@ -164,7 +164,7 @@ pub mod exchange {
                             .unwrap(),
                     };
 
-                    // validate price confidence - confidence/price ratio should be less than 1%
+                    // validate price confidence - confidence/price ratio should be less than 2.5%
                     let confidence: i64 = scaled_confidence.try_into().unwrap();
                     let confidence_100x = confidence.checked_mul(40).unwrap();
                     if confidence_100x > scaled_price {
