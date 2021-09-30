3e2a72a45daaa7d52c4d3346c5192443e52b1f65
Norbert Bodziony <norbertbodziony@gmail.com>
2021-09-30 12:42:45 +0200

    deploy

--- programs/exchange/src/lib.rs
+++ programs/exchange/src/lib.rs
@@ -166,7 +166,7 @@ pub mod exchange {
 
                     // validate price confidence - confidence/price ratio should be less than 1%
                     let confidence: i64 = scaled_confidence.try_into().unwrap();
-                    let confidence_100x = confidence.checked_mul(100).unwrap();
+                    let confidence_100x = confidence.checked_mul(40).unwrap();
                     if confidence_100x > scaled_price {
                         return Err(ErrorCode::PriceConfidenceOutOfRange.into());
                     };
