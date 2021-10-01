dc89cc5dd920af5b94a8aa0d8aa576420e264426
Norbert Bodziony <norbertbodziony@gmail.com>
2021-10-01 12:59:43 +0200

    add admin methods

---
+++ scripts/devnet/change-round-lenght copy.ts
@@ -0,0 +1,23 @@
+import { Provider } from '@project-serum/anchor'
+import { PublicKey, Transaction } from '@solana/web3.js'
+import { BN, Exchange, Network, signAndSend } from '@synthetify/sdk'
+import { DEVNET_ADMIN_ACCOUNT } from './admin'
+import { getLedgerWallet, signAndSendLedger } from '../walletProvider/wallet'
+import { percentToDecimal } from '@synthetify/sdk/lib/utils'
+
+const provider = Provider.local('https://api.devnet.solana.com', {
+  // preflightCommitment: 'max',
+  skipPreflight: true
+})
+const main = async () => {
+  const ledgerWallet = await getLedgerWallet()
+  const connection = provider.connection
+  // @ts-expect-error
+  const exchange = await Exchange.build(connection, Network.MAIN, DEVNET_ADMIN_ACCOUNT)
+  await exchange.getState()
+  const ix = await exchange.setStakingRoundLength(1209600)
+
+  const tx = await signAndSendLedger(new Transaction().add(ix), connection, ledgerWallet)
+  console.log(tx)
+}
+main()
