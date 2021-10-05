0c93e437fe8b36ef7f30107b080ce0fe033d062c
Wojciech Cichocki <cichockiwojciech97@gmail.com>
2021-10-05 07:48:21 +0200

    Change pyth addresses

--- programs/exchange/src/oracle.rs
+++ programs/exchange/src/oracle.rs
@@ -2,11 +2,11 @@ pub mod oracle {
     use anchor_lang::declare_id;
 
     #[cfg(feature = "mainnet")]
-    declare_id!("AHtgzX45WTKfkPG53L6WYhGEXwQkN1BVknET3sVsLL8J");
+    declare_id!("FsJ3A3u2vn5cTVofAjvy6y5kwABJAqYWpe4975bi2epH");
     #[cfg(feature = "testnet")]
-    declare_id!("AFmdnt9ng1uVxqCmqwQJDAYC5cKTkw8gJKSM5PnzuF6z");
+    declare_id!("8tfDNiaEyrV6Q1U4DEXrEigs9DoDtkugzFbybENEbCDz");
     #[cfg(feature = "devnet")]
-    declare_id!("BmA9Z6FjioHJPpjT39QazZyhDRUdZy2ezwx4GiDdE2u2");
+    declare_id!("gSbePebfvPy7tRqimPoVecS2UsBvYv46ynrzWocc92s");
     #[cfg(not(any(feature = "mainnet", feature = "testnet", feature = "devnet")))]
     declare_id!("3URDD3Eutw6SufPBzNm2dbwqwvQjRUFCtqkKVsjk3uSE");
 }
