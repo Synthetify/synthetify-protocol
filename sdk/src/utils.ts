35eeeaf991d833fa24343fbf89ac61ce39b67f71
Norbert Bodziony <norbertbodziony@gmail.com>
2021-12-29 14:00:42 +0100

    add xAVAX

--- sdk/src/utils.ts
+++ sdk/src/utils.ts
@@ -359,5 +359,6 @@ export const addressToAssetSymbol: { [key: string]: string } = {
   EzfgjvkSwthhgHaceR3LnKXUoRkP6NUhfghdaHAj1tUv: 'whFTT',
   '7vfCXTUXx5WJV5JADk17DUJ4ksgau7utNKj4b963voxs': 'whETH',
   mSoLzYCxHdYgdzU16g5QSh3i5K3z3KZK7ytfqcJm7So: 'mSOL',
-  '82Afat35Wr9v4fsZfSqGh8dnXFjxeaiQBfm5G9TK1BNj': 'xDOT'
+  '82Afat35Wr9v4fsZfSqGh8dnXFjxeaiQBfm5G9TK1BNj': 'xDOT',
+  HtxznfExBatdX28kMFDvmvU1rXVwiG3JSWcNPdFQ4PLh: 'xAVAX'
 }
